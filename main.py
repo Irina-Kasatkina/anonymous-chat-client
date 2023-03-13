e46c1c274b7cc5acc2c1bb2b9d9e96f286cccb73
Irina Kasatkina <kasatkina235@gmail.com>
2023-03-13 20:20:05 +0300

    Write chat messages to file

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import time
 from asyncio.streams import StreamReader, StreamWriter
 from contextlib import asynccontextmanager, suppress
 
+import aiofiles
 import configargparse
 from dotenv import load_dotenv
 
@@ -19,6 +20,7 @@ import defaults
 
 
 DELAY_PER_SECONDS = 1
+SLEEP_INTERVAL = 1 / 120
 
 
 def read_parse_args() -> argparse.Namespace:
@@ -43,6 +45,15 @@ def read_parse_args() -> argparse.Namespace:
         default=defaults.PORT_FOR_LISTENER,
         help='Порт для прослушивания сообщений из чата'
     )
+    parser.add(
+        '-f',
+        '--history-filepath',
+        metavar='FILEPATH',
+        type=str,
+        env_var='HISTORY_FILEPATH',
+        default=defaults.HISTORY_FILEPATH,
+        help=f'Путь к файлу для сохранения истории переписки'
+    )    
     return parser.parse_args()
 
 
@@ -50,20 +61,26 @@ async def main() -> None:
     """Инициализирует переменные и запускает программу ."""
 
     args = read_parse_args()
-    print(args)
 
     messages_queue = asyncio.Queue()
+    with suppress(FileNotFoundError):
+        with open(args.history_filepath, 'r', encoding='UTF8') as file_handler:
+            messages = file_handler.read().rstrip()
+        messages_queue.put_nowait(messages)
+
+    file_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
 
     await asyncio.gather(
         gui.draw(messages_queue, sending_queue, status_updates_queue),
-        read_msgs(args.host_for_listener, args.port_for_listener, messages_queue)
+        read_msgs(args.host_for_listener, args.port_for_listener, messages_queue, file_queue),
+        save_messages(args.history_filepath, file_queue)
     )
 
 
-async def read_msgs(host: str, port: int, queue: asyncio.Queue) -> None:
-    """Читает сообщения из чата."""
+async def read_msgs(host: str, port: int, queue: asyncio.Queue, file_queue: asyncio.Queue) -> None:
+    """Читает сообщения из чата и записывает их в две очереди."""
 
     while True:
         async with open_connection(host, port) as (reader, _):
@@ -72,7 +89,8 @@ async def read_msgs(host: str, port: int, queue: asyncio.Queue) -> None:
                     chat_message = await asyncio.wait_for(reader.readline(), timeout=10.0)
                     message = chat_message.decode().rstrip()
                     queue.put_nowait(message)
-                    await asyncio.sleep(1)
+                    file_queue.put_nowait(message)
+                    await asyncio.sleep(SLEEP_INTERVAL)
                 except asyncio.exceptions.TimeoutError:
                     return
 
@@ -97,8 +115,6 @@ async def open_connection(host: str, port: int) -> (StreamReader, StreamWriter):
                 logging.error(message)
                 break
 
-            message = 'Нет соединения с сервером'
-            logging.warning(message)
             await asyncio.sleep(DELAY_PER_SECONDS)
             continue
 
@@ -106,15 +122,21 @@ async def open_connection(host: str, port: int) -> (StreamReader, StreamWriter):
             if writer:
                 writer.close()
                 await writer.wait_closed()
-            message = 'Соединение с сервером закрыто'
-            logging.debug(message)
 
 
 def handle_keyboard_interrupt(signal, frame):
     """Обрабатывает прерывание работы программы с клавиатуры."""
-
-    logging.info('Работа программы была прервана с клавиатуры')
     exit(0)
+
+
+async def save_messages(filepath: str, queue: asyncio.Queue) -> None:
+    """Записывает сообщения в текстовый файл, находящийся по указанному пути."""
+
+    while True:
+        msg = await queue.get()
+        async with aiofiles.open(filepath, 'a', encoding='UTF8') as file_handler:
+            await file_handler.write(f'{msg}\n')
+        await asyncio.sleep(SLEEP_INTERVAL)
 
 
 if __name__ == '__main__':
