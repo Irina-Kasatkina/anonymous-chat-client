00c68e5559793ee11cd5fec45451e43e34bcba7a
Irina Kasatkina <kasatkina235@gmail.com>
2023-03-13 17:46:40 +0300

    Add command line arguments and environment variables

---
+++ defaults.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+"""Параметры программы по умолчанию."""
+
+HISTORY_FILEPATH = 'history.txt'
+HOST_FOR_LISTENER = 'minechat.dvmn.org'
+HOST_FOR_SENDER = 'minechat.dvmn.org'
+PORT_FOR_LISTENER = 5000
+PORT_FOR_SENDER = 5050
+USER_TOKEN_FILE = 'user_token.json'
--- main.py
+++ main.py
@@ -2,10 +2,66 @@
 
 """Запуск интерфейса пользователя чата."""
 
+import argparse
 import asyncio
 import gui
 import time
 from contextlib import suppress
+
+import configargparse
+from dotenv import load_dotenv
+
+import defaults
+
+
+def read_parse_args() -> argparse.Namespace:
+    """Принимает параметры из командной строки и файла .env."""
+    
+    load_dotenv()
+
+    parser = configargparse.ArgParser(description='Асинхронный клиент для подключения к чату')
+    parser.add(
+        '-f',
+        '--history-filepath',
+        metavar='FILEPATH',
+        type=str,
+        env_var='HISTORY_FILEPATH',
+        default=defaults.HISTORY_FILEPATH,
+        help=f'Путь к файлу для сохранения истории переписки'
+    )
+    parser.add(
+        '-hl',
+        '--host-for-listener',
+        type=str,
+        env_var='HOST_FOR_LISTENER',
+        default=defaults.HOST_FOR_LISTENER,
+        help='Хост для прослушивания сообщений из чата'
+    )
+    parser.add(
+        '-pl',
+        '--port-for-listener',
+        type=int,
+        env_var='PORT_FOR_LISTENER',
+        default=defaults.PORT_FOR_LISTENER,
+        help='Порт для прослушивания сообщений из чата'
+    )
+    return parser.parse_args()
+
+
+async def main() -> None:
+    """Инициализирует переменные и запускает программу ."""
+
+    args = read_parse_args()
+    print(args)
+
+    messages_queue = asyncio.Queue()
+    sending_queue = asyncio.Queue()
+    status_updates_queue = asyncio.Queue()
+
+    await asyncio.gather(
+        gui.draw(messages_queue, sending_queue, status_updates_queue),
+        generate_msgs(messages_queue)
+    )
 
 
 async def generate_msgs(queue: asyncio.Queue) -> None:
@@ -16,19 +72,6 @@ async def generate_msgs(queue: asyncio.Queue) -> None:
         await asyncio.sleep(1)
 
 
-async def main() -> None:
-    """Инициализирует переменные и запускает программу ."""
-
-    messages_queue = asyncio.Queue()
-    sending_queue = asyncio.Queue()
-    status_updates_queue = asyncio.Queue()
-
-    with suppress(gui.TkAppClosed, KeyboardInterrupt):
-        await asyncio.gather(
-            gui.draw(messages_queue, sending_queue, status_updates_queue),
-            generate_msgs(messages_queue)
-        )
-
-
 if __name__ == '__main__':
-    asyncio.run(main())
+    with suppress(gui.TkAppClosed, KeyboardInterrupt):
+        asyncio.run(main())
